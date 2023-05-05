336c4499abc3077ca8dd827c2bd84d659f2766f1
melkholy <28461793+SolarisCode@users.noreply.github.com>
2023-05-05 16:38:45 +0200

    Added Vim mode on the heirline bar

--- plugins/user.lua
+++ plugins/user.lua
@@ -32,4 +32,37 @@ return {
     "42Paris/42header",
     lazy = false,
   },
+  {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require("astronvim.utils.status")
+        opts.statusline = { -- statusline
+          hl = { fg = "fg", bg = "bg" },
+          status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+          -- status.component.file_info { filetype = false, filename = {}, file_modified = false },
+          status.component.file_info {
+            -- enable the file_icon and disable the highlighting based on filetype
+            file_icon = { padding = { left = 0 } },
+            filename = { fallback = "Empty" },
+            -- add padding
+            padding = { right = 1 },
+            -- define the section separator
+            surround = { separator = "left", condition = false },
+          },
+          status.component.git_branch(),
+          status.component.git_diff(),
+          status.component.diagnostics(),
+          status.component.fill(),
+          status.component.cmd_info(),
+          status.component.fill(),
+          status.component.lsp(),
+          status.component.treesitter(),
+          status.component.nav(),
+          -- remove the 2nd mode indicator on the right
+        }
+
+        -- return the final configuration table
+        return opts
+      end,
+    },
 }
