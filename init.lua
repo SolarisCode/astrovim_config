5733999ae64d39d68b90cb90888aaca7391a5822
melkholy <28461793+SolarisCode@users.noreply.github.com>
2023-05-07 21:18:28 +0200

    New theme

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  -- colorscheme = "catppuccin",
+  colorscheme = "kanagawa",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
